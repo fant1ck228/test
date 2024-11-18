8689c66790c6e4724f2edfbe58ca5c9a2c7eea63
fant1ck228 <migrylkadima@gmail.com>
2024-11-18 13:08:47 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.querySelectorAll('.detailsButton').forEach(button => {
+    button.addEventListener('click', () => {
+        const detailsContainer = button.closest('li').querySelector('.episodeDetails');
+        if (detailsContainer.classList.contains('showDetails')) {
+            detailsContainer.classList.remove('showDetails');
+        } else {
+            detailsContainer.classList.add('showDetails');
+        }
+    });
+});
 
